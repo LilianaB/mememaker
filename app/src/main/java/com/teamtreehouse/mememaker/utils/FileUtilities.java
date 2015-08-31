ad2c6f9bd96acd7b12c76352f0c9117bfe093b37
lbarrios <liliana.usb@gmail.com>
2015-08-31 11:57:23 +0200

    copy file form assets to storage

--- app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
+++ app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
@@ -12,6 +12,7 @@ import com.teamtreehouse.mememaker.MemeMakerApplicationSettings;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,7 +25,31 @@ import java.io.OutputStream;
 public class FileUtilities {
 
     public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);//give us file into file directory named assetName
 
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream in = assetManager.open(assetName); // input for reading file
+            FileOutputStream out = new FileOutputStream(fileDirectory);
+            copyFile(in, out);
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //FileOutputStream out2 = context.openFileOutput(fileToWrite.getAbsolutePath(), context.MODE_PRIVATE);//alternative
+        //second parameter make file readable writable outside app
+    }
+
+    //standard method to copy files
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
     }
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
